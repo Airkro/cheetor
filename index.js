e4eb10c8de1b6f4210c3591baf229ec317c48253
Eric Chen <airkro@qq.com>
2020-08-28 15:22:50 +0800

    fix

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
 const yargs = require('yargs');
-const { green } = require('chalk');
+const { green, level } = require('chalk');
 
 function requireFromMain(id) {
   // eslint-disable-next-line global-require, import/no-dynamic-require
   return require(require.resolve(id, { paths: [require.main.path] }));
 }
 
-function findCmd(cmd) {
+function findCmd(path) {
   try {
-    return requireFromMain(cmd);
+    return requireFromMain(path);
   } catch {
     return { command: '' };
   }
+}
+
+function ready() {
+  const { hasCommand, homepage, site = homepage, repository } = this;
+
+  const hasWebsite = site && site !== repository;
+
+  if (hasCommand) {
+    this.cli.usage(`Usage: ${green('$0')} <command>`);
+  } else {
+    this.cli.usage(`Usage: ${green('$0')}`);
+  }
+
+  this.cli
+    .epilogue(hasWebsite ? `Website: ${site}` : '\u001B[0m')
+    .epilogue(hasWebsite ? '\u001B[0m' : '');
 }
 
 module.exports = class Cheetor {
@@ -23,10 +39,10 @@ module.exports = class Cheetor {
       repository: { url } = {},
     } = requireFromMain(path);
 
-    const repository = url.replace(/\.git$/, '');
-    const hasWebsite = homepage && homepage !== repository;
-
-    [this.scriptName] = Object.keys(bin);
+    this.repository = url.replace(/\.git$/, '');
+    this.homepage = homepage;
+    // eslint-disable-next-line prefer-destructuring
+    this.scriptName = Object.keys(bin)[0];
 
     this.cli = yargs
       .strict()
@@ -36,11 +52,15 @@ module.exports = class Cheetor {
       .version(version)
       .hide('version')
       .detectLocale(false)
-      .epilogue(`Repository: ${repository}`)
-      .epilogue(hasWebsite ? `Website: ${homepage}` : '\u001B[0m')
-      .epilogue(hasWebsite ? '\u001B[0m' : '')
+      .epilogue(`Repository: ${this.repository}`)
       .scriptName(this.scriptName)
-      .usage('');
+      .usage('')
+      .option('color', {
+        coerce: () => level > 0,
+        describe: 'Colorful output',
+        type: 'boolean',
+        hide: true,
+      });
   }
 
   config(func) {
@@ -50,32 +70,36 @@ module.exports = class Cheetor {
 
   command(path) {
     this.hasCommand = true;
+    this.cli.command(requireFromMain(path));
+    return this;
+  }
+
+  commandSmart(path) {
+    this.hasCommand = true;
     this.cli.command(findCmd(path));
     return this;
   }
 
-  ready() {
-    if (this.hasCommand) {
-      this.cli.usage(`Usage: ${green('$0')} <command>`);
-    } else {
-      this.cli.usage(`Usage: ${green('$0')}`);
-    }
+  website(site) {
+    this.site = site;
+    return this;
+  }
+
+  effect(action) {
+    const { scriptName } = this;
+    action({ scriptName });
+    return this;
   }
 
   setup(action) {
-    this.ready();
+    ready.bind(this)();
 
     if (typeof action === 'function') {
-      process.title = this.scriptName;
-
       action(this.cli.argv);
     } else {
       if (this.hasCommand) {
         this.cli.demandCommand(1, "Won't work without a command");
       }
-
-      process.title = this.scriptName;
-
       // eslint-disable-next-line no-unused-expressions
       this.cli.argv;
     }
