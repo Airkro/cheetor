557586679a93016aa392356c1d16f1d108ff2571
Eric Chen <airkro@qq.com>
2021-07-06 22:30:14 +0800

    fix: path resolve

--- lib.mjs
+++ lib.mjs
@@ -1,21 +1,14 @@
-import { join } from 'path';
-import { fileURLToPath, pathToFileURL } from 'url';
 import { createRequire } from 'module';
-
-export function resolver(path, root) {
-  if (path.startsWith('.')) {
-    return join(root, path);
-  }
-  return path;
-}
+import { join } from 'path';
+import { fileURLToPath } from 'url';
 
 export function requireJson(path, root) {
   return createRequire(root)(path);
 }
 
 export function importFrom(path, root) {
-  const io = resolver(path, root);
-  return import(io.startsWith('file:') ? io : pathToFileURL(io));
+  const io = path.startsWith('.') ? join(root, path) : path;
+  return import(io);
 }
 
 export function importFromSafe(path, root) {
